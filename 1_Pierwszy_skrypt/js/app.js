dbd497ee47224965c17ec92112b9663d86413274
Luqi Map <luqimap@gmail.com>
2020-01-24 08:33:20 +0100

    Task 1

--- 1_Pierwszy_skrypt/js/app.js
+++ 1_Pierwszy_skrypt/js/app.js
@@ -1,1 +1,36 @@
+console.log("Hello world")
 
+/*Wpisz w pliku `js/app.js` następującą instrukcję:
+
+```JavaScript
+console.log("Twoje imię");
+```
+W konsoli wyswietla sie zawartosc console.log
+```js
+2 + 3
+```
+Poszczegolne dzielania sa wykonywane
+```js
+10 / 4
+```
+
+```js
+30 * 2
+```
+
+```js
+"Hello World"
+```
+Consola zwraca Hello World
+
+```js
+Hello World
+```
+Consola zwraca SyntaxError
+
+```js
+"Hello World
+```
+Consola nic nie zwraca, pozostaje jakby w cieglej pracy wykonywania polecenia
+
+*/
